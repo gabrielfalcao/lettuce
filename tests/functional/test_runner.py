850f489b8f2887e4a5e9dc3871b4f2ac7caa9ba3
Gabriel Falcão <gabriel@nacaolivre.org>
2010-04-20 05:14:55 -0300

    test runner

---
+++ tests/functional/test_runner.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# <Lettuce - Behaviour Driven Development for python>
+# Copyright (C) <2010>  Gabriel Falcão <gabriel@nacaolivre.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from os.path import dirname, abspath, join
+from nose.tools import assert_equals
+
+from lettuce import Runner
+from lettuce.terrain import before, world, after
+
+def test_output_with_success():
+    "Testing the output of a successful feature"
+
+    runner = Runner(join(abspath(dirname(__file__)), 'runner_features'))
+    runner.run()
