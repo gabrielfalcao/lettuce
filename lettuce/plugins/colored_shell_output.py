a4df8be7ce530467905fd7fab0f1d2a26deeb476
Gabriel Falcão <gabriel@nacaolivre.org>
2010-04-27 03:37:22 -0300

    fixing colored shell output of tables, its bars are now white :)

--- lettuce/plugins/colored_shell_output.py
+++ lettuce/plugins/colored_shell_output.py
@@ -23,51 +23,62 @@ from lettuce.terrain import before
 def wrap_file_and_line(string, start, end):
     return re.sub(r'([#] [^:]+[:]\d+)', '%s\g<1>%s' % (start, end), string)
 
+def wp(l):
+    if l.startswith("\033[1;32m"):
+        l = l.replace(" |", "\033[1;37m |\033[1;32m")
+    if l.startswith("\033[1;30m"):
+        l = l.replace(" |", "\033[1;37m |\033[1;30m")
+
+    return l
+
+def write_out(what):
+    sys.stdout.write(wp(what))
+
 @before.each_step
 def print_step_running(step):
     string = step.represent_string(step.sentence)
     string = wrap_file_and_line(string, '\033[1;30m', '\033[0m')
-    sys.stdout.write("\033[1;30m%s" % string)
+    write_out("\033[1;30m%s" % string)
     if step.data_list:
         for line in step.represent_data_list().splitlines():
-            sys.stdout.write("\033[1;30m%s\033[0m\n" % line)
+            write_out("\033[1;30m%s\033[0m\n" % line)
 
 @after.each_step
 def print_step_ran(step):
     if step.data_list:
-        sys.stdout.write("\033[A" * (len(step.data_list) + 1))
+        write_out("\033[A" * (len(step.data_list) + 1))
 
     string = step.represent_string(step.sentence)
     string = wrap_file_and_line(string, '\033[1;30m', '\033[0m')
-    sys.stdout.write("\033[A\033[1;32m%s" % string)
+    write_out("\033[A\033[1;32m%s" % string)
     if step.data_list:
         for line in step.represent_data_list().splitlines():
-            sys.stdout.write("\033[1;32m%s\033[0m\n" % line)
+            write_out("\033[1;32m%s\033[0m\n" % line)
 
 @before.each_scenario
 def print_scenario_running(scenario):
     string = scenario.represented()
     string = wrap_file_and_line(string, '\033[1;30m', '\033[0m')
-    sys.stdout.write("\033[1;37m%s" % string)
+    write_out("\033[1;37m%s" % string)
 
 @before.each_feature
 def print_feature_running(feature):
     string = feature.represented()
     lines = string.splitlines()
 
-    sys.stdout.write("\n")
+    write_out("\n")
     for line in lines:
         line = wrap_file_and_line(line, '\033[1;30m', '\033[0m')
-        sys.stdout.write("\033[1;37m%s\n" % line)
+        write_out("\033[1;37m%s\n" % line)
 
-    sys.stdout.write("\n")
+    write_out("\n")
 
 @after.all
 def print_end(total):
-    sys.stdout.write("\n")
+    write_out("\n")
 
     word = total.features_ran > 1 and "features" or "feature"
-    sys.stdout.write("\033[1;37m%d %s (\033[1;32m%d passed\033[1;37m)\033[0m\n" % (
+    write_out("\033[1;37m%d %s (\033[1;32m%d passed\033[1;37m)\033[0m\n" % (
         total.features_ran,
         word,
         total.features_passed
@@ -75,7 +86,7 @@ def print_end(total):
     )
 
     word = total.scenarios_ran > 1 and "scenarios" or "scenario"
-    sys.stdout.write("\033[1;37m%d %s (\033[1;32m%d passed\033[1;37m)\033[0m\n" % (
+    write_out("\033[1;37m%d %s (\033[1;32m%d passed\033[1;37m)\033[0m\n" % (
         total.scenarios_ran,
         word,
         total.scenarios_passed
@@ -83,7 +94,7 @@ def print_end(total):
     )
 
     word = total.steps > 1 and "steps" or "step"
-    sys.stdout.write("\033[1;37m%d %s (\033[1;32m%d passed\033[1;37m)\033[0m\n" % (
+    write_out("\033[1;37m%d %s (\033[1;32m%d passed\033[1;37m)\033[0m\n" % (
         total.steps,
         word,
         total.steps_passed
@@ -95,8 +106,8 @@ def print_no_features_found(where):
     if not where.startswith(os.sep):
         where = '.%s%s' % (os.sep, where)
 
-    sys.stdout.write('\033[1;31mOops!\033[0m\n')
-    sys.stdout.write(
+    write_out('\033[1;31mOops!\033[0m\n')
+    write_out(
         '\033[1;37mcould not find features at '
         '\033[1;33m%s\033[0m\n' % where
     )
