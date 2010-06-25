7ca858f4ca9fa98d7fb8b32951068a143d370f82
Gabriel Falcão <gabriel@nacaolivre.org>
2010-06-25 17:28:42 -0300

    release 0.1.8

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # lettuce
-> Version 0.1.6 - barium
+> Version 0.1.8 - barium
 
 ## On release names
 
--- lettuce/__init__.py
+++ lettuce/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-version = '0.1.7'
+version = '0.1.8'
 release = 'barium'
 
 import os
