4488bc9274d3504b5c73d5a21bb5accd2fcdc042
Gabriel Falcao <gabriel@nacaolivre.org>
2011-04-03 18:49:29 -0400

    small fix on terminal window size guessing. closes #121

--- tests/integration/test_grocery.py
+++ tests/integration/test_grocery.py
@@ -25,7 +25,7 @@ def test_django_admin_media_serving():
 
     FileSystem.pushd(current_directory, "django", "grocery")
 
-    status, out = commands.getstatusoutput("python manage.py harvest --verbosity=2 ./features/")
+    status, out = commands.getstatusoutput("python manage.py harvest --verbosity=3 ./features/")
     assert_equals(status, 0, out)
     FileSystem.popd()
 
