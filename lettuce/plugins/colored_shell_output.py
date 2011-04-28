ede8a9b4a2db04d5938882fbe61ea4717415765e
Gabriel Falcao <gabriel@nacaolivre.org>
2011-04-28 13:07:10 -0400

    improving the output of undefined steps containing tables

--- lettuce/plugins/colored_shell_output.py
+++ lettuce/plugins/colored_shell_output.py
@@ -63,7 +63,7 @@ def print_step_running(step):
     string = step.represent_string(step.original_sentence)
     string = wrap_file_and_line(string, '\033[1;30m', '\033[0m')
     write_out("%s%s" % (color, string))
-    if step.hashes:
+    if step.hashes and step.defined_at:
         for line in step.represent_hashes().splitlines():
             write_out("\033[1;30m%s\033[0m\n" % line)
 
@@ -72,7 +72,7 @@ def print_step_ran(step):
     if step.scenario.outlines:
         return
 
-    if step.hashes:
+    if step.hashes and step.defined_at:
         write_out("\033[A" * (len(step.hashes) + 1))
 
     string = step.represent_string(step.original_sentence)
