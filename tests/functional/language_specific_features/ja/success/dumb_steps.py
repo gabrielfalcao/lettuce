bc07750c014f0062ce98007f78c2b35c6503a3a9
Tetsuya Mitake <t.mitake@gmail.com>
2011-07-04 23:20:11 +0900

    add japanese support

---
+++ tests/functional/language_specific_features/ja/success/dumb_steps.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from lettuce import step
+
+@step(u'何もしない')
+def nanimo_shinai(step):
+    pass
---
+++ tests/functional/language_specific_features/ja/success/table_steps.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from lettuce import step
+
+@step(u'データは以下')
+def do_nothing_table(step):
+    pass
