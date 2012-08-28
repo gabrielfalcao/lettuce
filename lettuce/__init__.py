be50d4c0319079f67595662c6ee786b562db8716
Gabriel Falcao <gabriel@nacaolivre.org>
2012-08-28 16:06:13 -0400

    preparing next version

--- lettuce/__init__.py
+++ lettuce/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-version = '0.2.7'
+version = '0.2.8'
 release = 'kryptonite'
 
 import os
