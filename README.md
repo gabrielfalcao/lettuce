d380179b7049c68eb7707712fb9984e15858846b
Gabriel Falcao <gabriel@nacaolivre.org>
2013-01-28 13:18:42 -0500

    bumping to 0.2.13

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # lettuce
-> Version 0.2.12 - kryptonite
+> Version 0.2.13 - kryptonite
 
 ## On release names
 
--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ if os.name.lower() == 'nt':
 
 setup(
     name='lettuce',
-    version='0.2.12',
+    version='0.2.13',
     description='Behaviour Driven Development for python',
     author=u'Gabriel Falcao',
     author_email='gabriel@nacaolivre.org',
