55c87ab4a7980c5eb529b4a36c24ce56194d46b3
Danielle Madeley <danielle@madeley.id.au>
2013-07-07 17:25:23 +1000

    Include stderr and stdout in the stream

--- lettuce/plugins/subunit_output.py
+++ lettuce/plugins/subunit_output.py
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
+from cStringIO import StringIO
+
 from lettuce.terrain import before, after
 
 from subunit.v2 import StreamResultToBytes
@@ -48,14 +51,38 @@ def enable(filename=None):
     streamresult = StreamResultToBytes(file_)
     streamresult.startTestRun()
 
+    real_stdout = sys.stdout
+    real_stderr = sys.stderr
+
     @before.each_scenario
     def before_scenario(scenario):
 
+        # redirect stdout and stderr
+        sys.stdout = StringIO()
+        sys.stderr = StringIO()
         streamresult.status(test_id=get_test_id(scenario),
                             test_status='inprogress')
 
     @after.each_scenario
     def after_scenario(scenario):
+
+        streamresult.status(test_id=get_test_id(scenario),
+                            test_status='inprogress',
+                            file_name='stdout',
+                            file_bytes=sys.stdout.getvalue(),
+                            mime_type='text/plain; charset=utf8',
+                            eof=True)
+
+        streamresult.status(test_id=get_test_id(scenario),
+                            test_status='inprogress',
+                            file_name='stderr',
+                            file_bytes=sys.stderr.getvalue(),
+                            mime_type='text/plain; charset=utf8',
+                            eof=True)
+
+        # unredirect stdout and stderr
+        sys.stdout = real_stdout
+        sys.stderr = real_stderr
 
         if scenario.passed:
             streamresult.status(test_id=get_test_id(scenario),
